98e92a3d7d7d3ad124ce195b646c4a814f1adb95
nobii <fujisawa-shin@kayac.com>
2018-12-30 00:48:02 +0900

    aliasのconfigを追加

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -103,10 +103,11 @@ module.exports = {
       '.jsx',
     ],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '~': path.join(__dirname, '../src'),
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
